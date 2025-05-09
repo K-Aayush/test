e5a6abd980f96c71c8d7a4bca6cc9f348cd12090
Aayush <ayushkarkee6@gmail.com>
2025-05-09 14:46:49 +0545

    changes firebase middleware and firebase.js

--- src/middlewares/firebaseMiddleware.js
+++ src/middlewares/firebaseMiddleware.js
@@ -1,24 +1,50 @@
 const admin = require("../config/firebase");
 const GenRes = require("../utils/routers/GenRes");
+const { tokenGen } = require("../utils/auth/tokenHandler");
+const User = require("../modules/user/user.model");
 
 const firebaseMiddleware = async (req, res, next) => {
   req.user = null;
   try {
     const token = req?.headers?.authorization;
-    // doesnot occur still if reused seperately
     if (!token) {
       throw new Error("Token not found!");
     }
 
-    // decoded token
+    // Decode Firebase token
     const decodedToken = await admin.auth().verifyIdToken(token);
     if (!decodedToken) {
       throw new Error("Firebase failed to parse!");
     }
+
+    // Find or create user
+    const user = await User.findOne({ email: decodedToken.email });
+    if (!user) {
+      throw new Error("User not registered!");
+    }
+
+    // Generate JWT tokens
+    const tokenData = {
+      _id: user._id.toString(),
+      email: user.email,
+      phone: user.phone,
+      date: new Date(),
+    };
+
+    const { accessToken, refreshToken } = tokenGen(tokenData);
+
+    // Update user's refresh token
+    user.refreshToken = refreshToken;
+    await user.save();
+
+    // Add tokens to response headers
+    res.set("X-Access-Token", accessToken);
+    res.set("X-Refresh-Token", refreshToken);
+
     req.user = decodedToken;
     return next();
   } catch (error) {
-    const response = GenRes(500, null, error, error?.message)
+    const response = GenRes(500, null, error, error?.message);
     return res.status(500).json(response);
   }
 };
@@ -27,12 +53,10 @@ const registerMiddleware = async (req, _, next) => {
   req.user = null;
   try {
     const token = req?.headers?.authorization;
-    // doesnot occur still if reused seperately
     if (!token) {
       throw new Error("Token not found!");
     }
 
-    // decoded token
     const decodedToken = await admin.auth().verifyIdToken(token);
     if (!decodedToken) {
       throw new Error("Firebase failed to parse!");
