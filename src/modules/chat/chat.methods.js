5085dcf23db3827f5856d2b09c573ccff7a08e5e
Aayush <ayushkarkee6@gmail.com>
2025-05-11 16:26:23 +0545

    update :: chat encryption

---
+++ src/modules/chat/chat.methods.js
@@ -0,0 +1,13 @@
+const { isValidObjectId } = require("mongoose");
+const ChatMessage = require("./chat.model");
+const GenRes = require("../../utils/routers/GenRes");
+const Follow = require("../follow/follow.model");
+
+//method to get message
+const GetMessages = async (req, res) => {
+    try {
+        const userId = 
+    } catch (error) {
+        
+    }
+}
