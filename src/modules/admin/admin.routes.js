da9b7d4ba832eca5fce380984ca2839f72e8cc89
Aayush <ayushkarkee6@gmail.com>
2025-05-12 15:43:10 +0545

    normalized email

--- src/modules/admin/admin.routes.js
+++ src/modules/admin/admin.routes.js
@@ -1,13 +1,12 @@
 const loginAdmin = require("./admin.login");
 const { SaveAdmin, AddVendor } = require("./admin.methods");
 const registerAdmin = require("./admin.register");
+const { adminMiddleware } = require("../../middlewares/adminMiddleware");
 
 const route = require("express").Router();
 
 route.post("/admin-login", loginAdmin);
-
 route.post("/admin-register-request", registerAdmin);
-
 route.get("/validate-admin-login/:token", SaveAdmin);
 
 // Vendor management
