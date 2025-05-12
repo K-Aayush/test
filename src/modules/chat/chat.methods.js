b83a5f0726f991c6c71b239003e187b3ae82a1be
Aayush <ayushkarkee6@gmail.com>
2025-05-12 11:24:47 +0545

    fixed errors

--- src/modules/chat/chat.methods.js
+++ src/modules/chat/chat.methods.js
@@ -102,3 +102,126 @@ const GetMessages = async (req, res) => {
     return res.status(500).json(GenRes(500, null, error, error?.message));
   }
 };
+
+//Get chat list with latest messages
+const GetChats = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    //get mutual followers
+    const following = await Follow.find({ "follower._id": userId }).select(
+      "following"
+    );
+    const followers = await Follow.find({ "following._id": userId }).select(
+      "follower"
+    );
+
+    const mutalEmails = following
+      .map((f) => f.following.email)
+      .filter((email) => followers.some((f) => f.follower.email === email));
+
+    //Get latest chat message from each conversation
+    const chats = await ChatMessage.aggregate([
+      {
+        $match: {
+          $or: [
+            {
+              "sender._id": userId,
+              deletedBySender: false,
+              "receiver.email": { $in: mutalEmails },
+            },
+            {
+              "receiver._id": userId,
+              deletedByReceiver: false,
+              "sender.email": { $in: mutalEmails },
+            },
+          ],
+        },
+      },
+      {
+        $sort: { createdAt: -1 },
+      },
+      {
+        $group: {
+          _id: {
+            $cond: [
+              { $eq: ["$sender._id", userId] },
+              "$receiver._id",
+              "$sender._id",
+            ],
+          },
+          lastMessage: { $first: "$$ROOT" },
+          unreadCount: {
+            $sum: {
+              $cond: [
+                {
+                  $and: [
+                    { $eq: ["$receiver._id", userId] },
+                    { $eq: ["$read", false] },
+                  ],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+        },
+      },
+    ]);
+
+    //Decrypt last messages
+    const decryptedChats = chats.map((chat) => {
+      const msgDoc = new ChatMessage(chat.lastMessage);
+      chat.lastMessage.message = msgDoc.decryptMessage();
+      return chat;
+    });
+
+    return res
+      .status(200)
+      .json(
+        GenRes(
+          200,
+          decryptedChats,
+          null,
+          `Retrieved ${decryptedChats.length} chats`
+        )
+      );
+  } catch (error) {
+    return res.status(500).json(GenRes(500, null, error, error?.message));
+  }
+};
+
+//Delete message
+// const DeleteMessage = async (req, res) => {
+//   try {
+//     const messageId = req.params.messageId;
+//     const userId = req.user._id;
+
+//     if (!isValidObjectId(messageId)) {
+//       return res
+//         .status(400)
+//         .json(
+//           GenRes(
+//             400,
+//             null,
+//             { error: "Invalid message Id" },
+//             "Invalid message Id provided"
+//           )
+//         );
+//     }
+
+//     const message = await ChatMessage.findOne({ _id: messageId });
+//     if (!message) {
+//       return res
+//         .status(404)
+//         .json(
+//           GenRes(
+//             404,
+//             null,
+//             { error: "Message not found" },
+//             "Message not forund"
+//           )
+//         );
+//     }
+//   } catch (error) {}
+// };
