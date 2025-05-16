20091a8f6739312a9ea2bbf2e54ce015dbb17d53
Aayush <ayushkarkee6@gmail.com>
2025-05-16 15:24:28 +0545

    multiple origins

--- index.js
+++ index.js
@@ -16,10 +16,20 @@ const server = http.createServer(app);
 // Create MQTT server
 const mqttServer = net.createServer(aedes.handle);
 
+// List of allowed origins
+const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:3000"];
+
 // use middlewares
 app.use(
   cors({
-    origin: process.env.WEB_HOST || "*",
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   }),
@@ -27,6 +37,7 @@ app.use(
   express.urlencoded({ extended: true, limit: "200gb" })
 );
 
+// Static file routes
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use("/shop", express.static(path.join(process.cwd(), "shop")));
 app.use(
@@ -34,10 +45,10 @@ app.use(
   express.static(path.join(process.cwd(), "courses/public"))
 );
 
-// configs
+// Database connection
 DB();
 
-// calling main App
+// Main application routes
 App(app);
 
 // Start MQTT server
@@ -46,6 +57,7 @@ mqttServer.listen(MQTT_PORT, () => {
   console.log(`MQTT broker running on port ${MQTT_PORT}`);
 });
 
+// Start HTTP server
 server.listen(process.env.PORT, (error) => {
   if (error) {
     console.error("Error in listening Server:", error);
